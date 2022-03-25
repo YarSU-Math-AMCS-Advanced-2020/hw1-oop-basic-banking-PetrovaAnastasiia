6c38044165d8e2ffdc77e8f7aac34313fe0b4346
PetrovaAnastasiia <76555541+PetrovaAnastasiia@users.noreply.github.com>
2022-03-25 21:51:06 +0300

    Delete BankAccount.cpp

--- BankAccount.cpp
+++
@@ -1,7 +0,0 @@
-#include "BankAccount.h"
-
-BankAccount::BankAccount(string account_id, double balance, string phone_number) {
-	set_account_id(account_id);
-	set_balance(balance);
-	set_phone_number(phone_number);
-}
